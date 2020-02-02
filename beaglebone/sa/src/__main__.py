5b645f839bf70341ae8d62fa41b4d9a5affa26ff
unknown <eshimoun@umich.edu>
2020-02-02 14:18:24 -0500

    Updated servo file to work with new servos and command file

--- beaglebone/sa/src/__main__.py
+++ beaglebone/sa/src/__main__.py
@@ -2,7 +2,7 @@ import asyncio
 import time
 import Adafruit_BBIO.PWM as PWM
 from rover_common import aiolcm
-from rover_msgs import Servo
+from rover_msgs import ServoCmd
 
 SERVO_MAX_DC = 10.0
 SERVO_MIN_DC = 4.0
@@ -34,19 +34,19 @@ def servo_init(pin, degrees):
     PWM.start(pin, dc, 50)
 
 def servo_callback(channel, msg):
-    servo = Servo.decode(msg)
+    servo = ServoCmd.decode(msg)
     if servo.id == "servo_1":
-        run_servo(servos[0], servo.degrees)
+        run_servo(servos[0], servo.position)
     elif servo.id == "servo_2":
-        run_servo(servos[1], servo.degrees)
+        run_servo(servos[1], servo.position)
     elif servo.id == "servo_3":
-        run_servo(servos[2], servo.degrees)
+        run_servo(servos[2], servo.position)
     elif servo.id == "servo_4":
-        run_servo(servos[3], servo.degrees)
+        run_servo(servos[3], servo.position)
     elif servo.id == "servo_5":
-        run_servo(servos[4], servo.degrees)
+        run_servo(servos[4], servo.position)
     elif servo.id == "servo_6":
-        run_servo(servos[5], servo.degrees)
+        run_servo(servos[5], servo.position)
     else:
         print("Invalid servo ID.")
 
@@ -54,5 +54,5 @@ def main():
     for pin in servos:
         servo_init(pin, 0)
 
-    lcm_.subscribe("/servo", servo_callback)
+    lcm_.subscribe("/servo_cmd", servo_callback)
     
